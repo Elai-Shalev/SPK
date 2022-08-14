c29f3326f561ddd4c9e0e3d7aa09663976a6c8b0
IdoMeshulam <idomeshulam@mail.tau.ac.il>
2022-08-14 15:23:41 +0300

    added globals, def and macros

--- spkmeans.c
+++ spkmeans.c
@@ -1,3 +1,26 @@
+#define PY_SSIZE_T_CLEAN
+// include "Python.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+
+/* Global Variables */
+int MAX_ITER;
+int K;
+double const EPSILON;
+int dim;
+int num_of_vectors;
+
+/* Macros */
+#define SQR(x) ((x)*(x))
+
+/* Structs */
+typedef struct {
+    double* coordinate;
+    int cluster;
+} Vector;
+
 //Read the File
 //Matricies are flattened arrays 
 
@@ -9,7 +32,6 @@ double* calc_lnorm_matrix(double* mat, int n){
 }
 double* calc_eigen(double* mat, int n){
 }
-
 
 
 int main(int argc, char* args[]){
