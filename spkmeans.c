5ac54b3d8d3fae7f93877d048ec5a33669b85017
Elai Shalev <elaishalev@mail.tau.ac.il>
2022-08-14 15:18:28 +0300

    spkmeans1 commit

--- spkmeans.c
+++ spkmeans.c
@@ -1,3 +1,17 @@
+//Read the File
+//Matricies are flattened arrays 
+
+double* calc_weighted_matrix(double* points, int n){
+}
+double* calc_diagonal_deg_matrix(double* mat, int n){
+}
+double* calc_lnorm_matrix(double* mat, int n){
+}
+double* calc_eigen(double* mat, int n){
+}
+
+
+
 int main(int argc, char* args[]){
     return 0;
 }
